4f0ff36069dd8fe38ad4f345ca65a4d2a9ca863f
delfiboyz <delfiboyz@gmail.com>
2023-06-09 23:50:28 +0700

    Update play again

--- SnakeGame.py
+++ SnakeGame.py
@@ -41,14 +41,55 @@ y_change = 0
 food_x = round(random.randrange(0, screen_width - snake_block_size) / 20.0) * 20.0
 food_y = round(random.randrange(0, screen_height - snake_block_size) / 20.0) * 20.0
 
+# Skor
+score = 0
+
+# Font skor
+font_style = pygame.font.SysFont(None, 30)
+
+# Font game over
+game_over_font = pygame.font.SysFont(None, 48)
+
+# Fungsi untuk menampilkan pesan "Game Over"
+def show_game_over_screen():
+    game_over_text = game_over_font.render("Game Over", True, RED)
+    play_again_text = font_style.render("Press SPACE to Play Again", True, WHITE)
+    screen.blit(game_over_text, (screen_width // 2 - game_over_text.get_width() // 2, screen_height // 2 - game_over_text.get_height() // 2))
+    screen.blit(play_again_text, (screen_width // 2 - play_again_text.get_width() // 2, screen_height // 2 + game_over_text.get_height() // 2))
+    pygame.display.update()
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    waiting = False
+                    play_again()
+
+# Fungsi untuk memulai permainan kembali
+def play_again():
+    global snake_x, snake_y, snake_block, snake_length, x_change, y_change, score, game_over
+    snake_block.clear()
+    snake_length = 1
+    snake_x = screen_width // 2
+    snake_y = screen_height // 2
+    snake_block.append((snake_x, snake_y))
+    x_change = 0
+    y_change = 0
+    score = 0
+    game_over = False
+
 # Permainan berjalan
 game_over = False
 
 # Loop utama
-while not game_over:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_over = True
+            pygame.quit()
+            quit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 x_change = -snake_block_size
@@ -89,6 +130,10 @@ while not game_over:
     for segment in snake_block:
         pygame.draw.rect(screen, BLUE, [segment[0], segment[1], snake_block_size, snake_block_size])
 
+    # Menampilkan skor
+    score_text = font_style.render("Score: " + str(score), True, WHITE)
+    screen.blit(score_text, (10, 10))
+
     # Update layar
     pygame.display.update()
 
@@ -97,11 +142,13 @@ while not game_over:
         food_x = round(random.randrange(0, screen_width - snake_block_size) / 20.0) * 20.0
         food_y = round(random.randrange(0, screen_height - snake_block_size) / 20.0) * 20.0
         snake_length += 1
+        score += 1
 
     # Kecepatan permainan
     clock = pygame.time.Clock()
     clock.tick(snake_speed)
 
-# Berakhirnya permainan
-pygame.quit()
-quit()
+    # Mengecek jika game over
+    if game_over:
+        show_game_over_screen()
+        play_again()
